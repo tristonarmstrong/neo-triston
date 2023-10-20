958807cd94a31e5dd6820c7b463c6371966718fb
Triston Armstrong <triston95strong@gmail.com>
2023-10-20 15:35:47 -0500

    (untested) reject if on windows

--- install.sh
+++ install.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+# Check if the OS is Windows and exit with a message
+if [ -n "$OS" ]; then
+    echo "Ew... Windows? I just puked a little..."
+    exit 1
+fi
+
 # Define the source and destination directories
-source_dir=$(pwd)                  # Current directory
+source_dir="$(pwd)"  # Current directory
 destination_dir="$HOME/.config/nvim"
 
-# Check if the destination directory exists, and if not, create it
+# Check if the destination directory exists and create it if it doesn't
 if [ ! -d "$destination_dir" ]; then
-    mkdir -p "$destination_dir"
+    if [ -d "$HOME/.config" ]; then
+        mkdir -p "$destination_dir"
+    else
+        echo "Config directory ($HOME/.config) does not exist. Please create it manually."
+        exit 1
+    fi
 fi
 
 # Move all the files and directories from the current directory to the destination directory
